d0733370495eccfef95e2f878c994c75b1472961
Luke Ellis <lke8@cornell.edu>
2021-09-25 20:22:52 -0400

    Initial Commit

---
+++ Spacewalk2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spacewalk2
+//
+//  Created by Luke Ellis on 9/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spacewalk2/Spacewalk2App.swift
@@ -0,0 +1,17 @@
+//
+//  Spacewalk2App.swift
+//  Spacewalk2
+//
+//  Created by Luke Ellis on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Spacewalk2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
